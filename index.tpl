5e6ff0bfb95a93411ae1be865ba6fe8afa15f5cd
danxinwu <1299754315@qq.com>
2018-09-19 15:43:03 +0800

    打包修改

--- index.tpl
+++ index.tpl
@@ -5,9 +5,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>caonimei</title>
-<link href="/dist/style.css" rel="stylesheet"></head>
+</head>
 <body>
   <div id="app">
   </div>
-<script type="text/javascript" src="/dist/vendor.js"></script><script type="text/javascript" src="/dist/main.js"></script></body>
+</body>
 </html>
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,25 @@
-// 由于 webpack 是基于Node进行构建的，所有，webpack的配置文件中，任何合法的Node代码都是支持的
-var path = require('path')
-var buildFloder = 'dist'
+var proxyApi = '' // 代理服务器
+// 先清空 build 文件夹下的文件
+var buildFolder = 'dist'
 var fs = require('fs')
-var buildPath = './' + buildFloder + '/'
+var path = require('path')
+var buildPath = './' + buildFolder + '/'
 var folder_exists = fs.existsSync(buildPath)
-if(!folder_exists) {
-  fs.mkdirSync(buildPath)
-}else {
-  var dirList = fs.readdirSync(buildPath)
-  console.log(dirList)
-  dirList.forEach(function(fileName) {
-    fs.unlinkSync(buildPath + fileName)
-  })
-  console.log("clearimg" + buildPath)
 
+if (!folder_exists) {
+    fs.mkdirSync(buildFolder)
 }
-fs.readFileSync('index.html','utf-8',function (err, data) {
+else {
+    var dirList = fs.readdirSync(buildPath)
+    dirList.forEach(function (fileName) {
+        fs.unlinkSync(buildPath + fileName)
+    })
+    console.log("clearing " + buildPath)
+}
+
+// readfile
+// 先把index.html里面关于style和js的hash值都删除掉，避免在使用 npm run dev 的时候，路径还是压缩后的路径
+fs.readFile('index.html','utf-8',function (err, data) {
     if (err) {
         console.log("clear hash error")
     } else {
@@ -23,54 +27,85 @@ fs.readFileSync('index.html','utf-8',function (err, data) {
         fs.writeFileSync('index.html',devhtml)
     }
 })
+// 在内存中，根据指定的模板页面，生成一份内存中的首页，同时自动把打包好的bundle注入到页面底部
+// 如果要配置插件，需要在导出的对象中，挂载一个 plugins 节点
+var htmlWebpackPlugin = require('html-webpack-plugin')
 var webpack = require('webpack')
 
 // 检测重用模块
 var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin
+
 // 独立样式文件
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
-// 在内存中，根据指定的模板页面，生成一份内存中的首页，同时自动把打包好的bundle注入到页面底部
-// 如果要配置插件，需要在导出的对象中，挂载一个 plugins 节点
-var htmlWebpackPlugin = require('html-webpack-plugin')
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+// 在命令行 输入  “PRODUCTION=1 webpack --progress” 就会打包压缩，并且注入md5戳 到 d.html里面
+
+var production = process.env.PRODUCTION
 // 会将所有的样式文件打包成一个单独的style.css
 var extractCSS = new ExtractTextPlugin({
-    filename: 'style.[chunkhash].css',
+    filename: production ? 'style.[chunkhash].css' : 'style.css',
     disable: false,
     allChunks: true
 })
+var HtmlWebpackPlugin = new htmlWebpackPlugin({
+    filename: '../index.html', // 会生成d.html在根目录下,并注入脚本
+    template: 'index.tpl',
+    inject: true //此参数必须加上，不加不注入
+})
+var plugins = [
+    new webpack.LoaderOptionsPlugin({
+        minimize: true
+    }),
+    extractCSS,
+    HtmlWebpackPlugin,
+    // 自动分析重用模块并且打包单独文件
+    new CommonsChunkPlugin(production ? 'vendor' : 'vendor'),
+    function () {
+        return this.plugin('done', function (stats) {
+            var content
+            //这里可以拿到hash值   参考：http://webpack.github.io/docs/long-term-caching.html
+            content = JSON.stringify(stats.toJson().assetsByChunkName, null, 2)
+            console.log('版本是：'+JSON.stringify(stats.toJson().hash))
+            return fs.writeFileSync(buildFolder + '/assets.json', content)
+        })
+    }
+]
+/*plugins.push(new htmlWebpackPlugin({
+    filename: '../index.html', // 会生成d.html在根目录下,并注入脚本
+    template: 'index.tpl',
+    inject: true //此参数必须加上，不加不注入
+}))*/
 // 当以命令行形式运行 webpack 或 webpack-dev-server 的时候，工具会发现，我们并没有提供 要打包 的文件的 入口 和 出口文件，此时，他会检查项目根目录中的配置文件，并读取这个文件，就拿到了导出的这个 配置对象，然后根据这个对象，进行打包构建
 module.exports = {
   entry: path.join(__dirname, './src/main.js'), // 入口文件
   output: { // 指定输出选项
     path: path.join(__dirname, './dist'), // 输出路径
-    publicPath: '/dist/',
-    filename: '[name].[chunkhash].js' // 指定输出文件的名称
+    publicPath: '/' + buildFolder + '/',
+    filename: production ? '[name].[chunkhash].js' : '[name].js' // 指定输出文件的名称
   },
-  plugins: [ // 所有webpack  插件的配置节点
+  plugins: plugins,
+/*  plugins: [ // 所有webpack  插件的配置节点
     new htmlWebpackPlugin({
-      template: path.join(__dirname, './index.tpl'), // 指定模板文件路径
-      filename: '../index.html', // 设置生成的内存页面的名称
-      inject: true //此参数必须加上，不加不注入
-    }),
-    new CommonsChunkPlugin('vendor'),
-    extractCSS
-  ],
+      template: path.join(__dirname, './index.html'), // 指定模板文件路径
+      filename: 'index.html' // 设置生成的内存页面的名称
+    })
+  ],*/
   module: { // 配置所有第三方loader 模块的
     rules: [ // 第三方模块的匹配规则
       { test: /\.css$/, use: ['style-loader', 'css-loader'] }, // 处理 CSS 文件的 loader
       { test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader'] }, // 处理 less 文件的 loader
       { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] }, // 处理 scss 文件的 loader
-      { test: /\.(jpg|png|gif|bmp|jpeg)$/, use: 'url-loader?limit=76310&name=[hash:8]-[name].[ext]' }, // 处理 图片路径的 loader
+      { test: /\.(jpg|png|gif|bmp|jpeg)$/, use: 'url-loader?limit=7631&name=[hash:8]-[name].[ext]' }, // 处理 图片路径的 loader
       // limit 给定的值，是图片的大小，单位是 byte， 如果我们引用的 图片，大于或等于给定的 limit值，则不会被转为base64格式的字符串， 如果 图片小于给定的 limit 值，则会被转为 base64的字符串
       { test: /\.(ttf|eot|svg|woff|woff2)$/, use: 'url-loader' }, // 处理 字体文件的 loader 
       { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ }, // 配置 Babel 来转换高级的ES语法
       { 
-
-        test: /\.vue$/, 
+        test: /\.vue$/,
         loader: 'vue-loader',
         options: {
-            extractCSS: extractCSS
-        } 
+          extractCSS: extractCSS
+        }
       } // 处理 .vue 文件的 loader
     ]
   },
